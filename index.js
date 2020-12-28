fcb3710306e640e206394eee6917ba4a93fa2f23
tamir124 <tamirgez@gmail.com>
2020-12-28 03:49:20 +0200

    added some skeleton for future json object

--- index.js
+++ index.js
@@ -2,10 +2,20 @@ const Oldfs = require('fs');
 const fs = Oldfs.promises;
 const {Helper} = require('dxf');
 const util = require('util');
+const { type } = require('os');
 
+function currentDate(){
+    let today = new Date();
+    let dd = String(today.getDate()).padStart(2, '0');
+    let mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+    let yyyy = today.getFullYear();
+
+    return yyyy + '.' + mm + '.' + dd;
+}
 async function changeMe(){
     try{
-        dataString = await fs.readFile("./job description/dxf_example.dxf","utf8");
+        let fileName = "dxf_example.dxf";
+        dataString = await fs.readFile(`./job description/${fileName}`,"utf8");
         //console.log(dataString.substring(0,100));
         const helper = new Helper(dataString);
         theObject = helper.parsed.entities;
@@ -17,10 +27,33 @@ async function changeMe(){
         console.log(theObject[2].layer);//*logs a specific entity's layer name
         const requestedLayer = "PMISUNDER";
         const result = theObject.filter(key => key.layer == requestedLayer);
-        console.log(result);
-        //console.dir(helper.parsed);
+        filteredObj = {
+            "version":
+            {
+                "title": "DXF Converted to JSON",
+                "objectName": fileName,
+                "versionTimestamp": "2020.28.12"
+            },
+            "metadata":"bebe",
+            "layersInDxfSource":"",
+            "layerFromDxfSource":
+            {
+                "layername":"",
+                "code_00_drawingType"    : result[0]?.type,
+				"code_10_startXeastLng"  : "string",
+				"code_20_startYnorthLat" : "string",
+				"code_30_startZelevation": "string",
+				"code_11_endXeastLng"    : "string",
+				"code_21_endYnorthLat"   : "string",
+				"code_31_endZelevation"  : "string",
+				"code_40_circleArcRadius": "string",
+				"code_50_startArcAngle"  : "string",
+				"code_51_endArcAngle"    : "string"
+            }
+        } 
+        console.dir(filteredObj);   
+        //console.log(result);
         fs.writeFile("./job description/dxf_example_myConvert.txt",JSON.stringify(helper.parsed));
-        //fs.writeFile("./job description/dxf_example_myConvert.txt",theObject);
     }
     catch(error){
         console.error(error);
