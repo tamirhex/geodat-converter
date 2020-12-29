be139a35032b8380d48995c7492b305d0d2e6746
tamir124 <tamirgez@gmail.com>
2020-12-29 03:50:38 +0200

    working dxf convert from url using get request

---
+++ DxfToJson.js
@@ -0,0 +1,73 @@
+const Oldfs = require('fs');
+const fs = Oldfs.promises;
+const {Helper} = require('dxf');
+const util = require('util');
+const request = require('request');
+const axios = require('axios');
+//url to download dxf:
+//https://firebasestorage.googleapis.com/v0/b/webqpm-client-dev.appspot.com/o/files%2Fdxf_example.dxf?alt=media&token=01de6805-5deb-44ca-9e64-66b9789066a3
+
+exports.DxfToJson = async (url,layerName) => {
+    try{
+        //*reads the dxf file preparing for parsing*//
+        //dataString = await fs.readFile(url,"utf8");
+        /*const options = {
+            url: 'https://firebasestorage.googleapis.com/v0/b/webqpm-client-dev.appspot.com/o/files%2Fdxf_example.dxf?alt=media&token=01de6805-5deb-44ca-9e64-66b9789066a3',
+
+        }*/
+        dxfContents =  await axios.get("https://firebasestorage.googleapis.com/v0/b/webqpm-client-dev.appspot.com/o/files%2Fdxf_example.dxf?alt=media&token=01de6805-5deb-44ca-9e64-66b9789066a3");
+        dataString = dxfContents.data;
+        //let log = util.inspect(dataString, false, null);
+        let log = dataString;
+        fs.writeFile("./job description/deleteme.txt",log);
+        //* parsing the dxf recieving a parsed object  *//
+        const helper = new Helper(dataString);
+        theObject = helper.parsed.entities;
+
+        //* filters the dxf for requested layers and types within a layer *//
+        const requestedLayers = [layerName];
+        const requestedTypes = ["LINE"];
+        //* filters the dxf for requested layers and types within a layer *//
+        const result = theObject.filter(key => requestedLayers[0] && //*requestedLayers[0] because currently only possible to request a single layer from API.
+                    requestedTypes.includes(key.type));
+        //* building the initial object of layer before inserting all drawings of layer*//
+        filteredObj = {
+            "version":
+            {
+                "title":"DXF Converted to JSON",
+                "objectName":url,
+                "versionTimestamp":"2020.29.12"
+            },
+            "metadata":"Will be added later",
+            "layersInDxfSource":"will be added later",
+            "layerFromDxfSource":
+            {
+                "layerName":requestedLayers[0],
+                "layerDrawings":[],
+            }
+        }    
+        //*fills layerDrawings array with json objects describing the drawings*//
+        for(i in result){
+            filteredObj.layerFromDxfSource.layerDrawings[i] = 
+            {
+                "code_00_drawingType":result[i]?.type,
+                "code_10_startXeastLng":result[i]?.start?.x,
+                "code_20_startYnorthLat":result[i]?.start?.y,
+                "code_30_startZelevation":result[i]?.start?.z,
+                "code_11_endXeastLng":result[i]?.end?.x,
+                "code_21_endYnorthLat":result[i]?.end?.y,
+                "code_31_endZelevation":result[i]?.end?.z,
+                "code_40_circleArcRadius":result[i]?.r,
+                "code_50_startArcAngle":result[i]?.startAngle,
+                "code_51_endArcAngle":result[i]?.endAngle
+            }
+
+        }
+        //* Writes the object as a nice string and writes to file *//
+        let data = util.inspect(filteredObj, false, null);
+        fs.writeFile("./job description/dxf_example_myConvert.txt",data);
+    }
+    catch(error){
+        console.error(error);
+    }   
+}
--- index.js
+++ index.js
@@ -1,7 +1,9 @@
+const {DxfToJson}= require("./DxfToJson");
 const Oldfs = require('fs');
 const fs = Oldfs.promises;
 const {Helper} = require('dxf');
 const util = require('util');
+const request = require('request');
 
 
 function currentDate(){
@@ -13,61 +15,15 @@ function currentDate(){
     return yyyy + '.' + mm + '.' + dd;
 }
 
-async function changeMe(){
-    try{
-        //*reads the dxf file preparing for parsing*//
-        let fileName = "dxf_example.dxf";
-        dataString = await fs.readFile(`./job description/${fileName}`,"utf8");
 
-        //* parsing the dxf recieving a parsed object  *//
-        const helper = new Helper(dataString);
-        theObject = helper.parsed.entities;
 
-        //* filters the dxf for requested layers and types within a layer *//
-        const requestedLayers = ["PMISUNDER"];
-        const requestedTypes = ["LINE"];
-        //* filters the dxf for requested layers and types within a layer *//
-        const result = theObject.filter(key => requestedLayers[0] && //*requestedLayers[0] because currently only possible to request a single layer from API.
-                    requestedTypes.includes(key.type));
-        //* building the initial object of layer before inserting all drawings of layer*//
-        filteredObj = {
-            "version":
-            {
-                "title":"DXF Converted to JSON",
-                "objectName":fileName,
-                "versionTimestamp":"2020.28.12"
-            },
-            "metadata":"Will be added later",
-            "layersInDxfSource":"will be added later",
-            "layerFromDxfSource":
-            {
-                "layerName":requestedLayers[0],
-                "layerDrawings":[],
-            }
-        }    
-        //*fills layerDrawings array with json objects describing the drawings*//
-        for(i in result){
-            filteredObj.layerFromDxfSource.layerDrawings[i] = 
-            {
-                "code_00_drawingType":result[i]?.type,
-                "code_10_startXeastLng":result[i]?.start?.x,
-                "code_20_startYnorthLat":result[i]?.start?.y,
-                "code_30_startZelevation":result[i]?.start?.z,
-                "code_11_endXeastLng":result[i]?.end?.x,
-                "code_21_endYnorthLat":result[i]?.end?.y,
-                "code_31_endZelevation":result[i]?.end?.z,
-                "code_40_circleArcRadius":result[i]?.r,
-                "code_50_startArcAngle":result[i]?.startAngle,
-                "code_51_endArcAngle":result[i]?.endAngle
-            }
-
-        }
-        //* Writes the object as a nice string and writes to file *//
-        let data = util.inspect(filteredObj, false, null);
-        fs.writeFile("./job description/dxf_example_myConvert.txt",data);
-    }
-    catch(error){
-        console.error(error);
-    }
+let fileName = "dxf_example.dxf";
+url = `./job description/${fileName}`;
+async function banana(){
+dataString = await request.get("https://firebasestorage.googleapis.com/v0/b/webqpm-client-dev.appspot.com/o/files%2Fdxf_example.dxf?alt=media&token=01de6805-5deb-44ca-9e64-66b9789066a3");
+console.log(dataString);
+fs.writeFile("./job description/deleteMe.txt",dataString);
 }
-changeMe();
+
+DxfToJson(url,"PMISUNDER");
+
